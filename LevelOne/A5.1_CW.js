33659ef219505e342b430c100c56fc0057d15753
Ajay kumar <ajcoder25@gmail.com>
2024-12-28 20:10:15 +0530

    forLoop

---
+++ LevelOne/A5.1_CW.js
@@ -0,0 +1,62 @@
+console.log('A5.1_CW')
+console.log('---- ---- ----')
+console.log('1.1')
+console.log('---- ---- ----')
+
+for(let i = 1; i<=10; i++){
+  console.log(i)
+}
+
+console.log('---- ---- ----')
+console.log('1.2')
+console.log('---- ---- ----')
+
+let pattern = ''
+for(let i = 1; i<=4; i++){
+  pattern = pattern + 1
+  console.log(pattern)
+}
+
+ console.log('---- ---- ----')
+ console.log('1.3')
+console.log('---- ---- ----')
+
+let newPattern = ''
+for(let i = 1; i<=5; i++){
+
+  newPattern = newPattern + 'a'
+  console.log(newPattern)
+}
+
+console.log('---- ---- ----')
+console.log('1.4')
+console.log('---- ---- ----')
+
+let numberPattern = ''
+for(let i = 1; i<=5; i++){
+
+ numberPattern = numberPattern + i
+
+  console.log(numberPattern)
+}
+
+console.log('---- ---- ----')
+console.log('1.5')
+console.log('---- ---- ----')
+
+for(let i = 1; i <=10; i ++){
+  let result = 2*i
+  let table = '2 ' +'* ' + i + ' = ' + result
+
+  console.log(table)
+}
+
+console.log('---- ---- ----')
+
+console.log('1.6')
+console.log('---- ---- ----')
+
+for(let i = 1; i<=10; i++){
+  let square = i + ' * ' + i +' = ' + i * i
+  console.log(square)
+}
