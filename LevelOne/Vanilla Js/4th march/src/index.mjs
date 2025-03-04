8a13c0c26c9c6ace66de5e681e5b745e08f42d4e
Ajay kumar <ajcoder25@gmail.com>
2025-03-04 20:53:52 +0530

    4th march

---
+++ LevelOne/Vanilla Js/4th march/src/index.mjs
@@ -0,0 +1,36 @@
+const movies = [
+  { id: 1, title: "Movie 1", genre: "action" },
+  { id: 2, title: "Movie 1", genre: "comedy" },
+  { id: 3, title: "Movie 1", genre: "drama" },
+  { id: 4, title: "Movie 1", genre: "action" },
+  { id: 5, title: "Movie 1", genre: "comedy" },
+];
+
+const movieListContainer = document.querySelector("#movieList");
+const filterRadioButtons = document.getElementsByName("genre");
+
+function renderMovies(selectedGenre) {
+  const filteredMovies =
+    selectedGenre === "all"
+      ? movies
+      : movies.filter((movie) => movie.genre === selectedGenre);
+
+  const movieListHTML = filteredMovies.map(
+    (movie) => `<li><strong>ID:</strong> ${movie.id}<br>
+          <strong>Title:</strong> ${movie.title}<br>
+          <strong>Genre:</strong>${movie.genre}<br>
+          <hr>
+          </li>
+       
+      `
+  );
+  movieListContainer.innerHTML = movieListHTML.join(" ");
+}
+
+renderMovies("all");
+
+filterRadioButtons.forEach((radioBtn) => {
+  radioBtn.addEventListener("change", function () {
+    renderMovies(this.value);
+  });
+});
